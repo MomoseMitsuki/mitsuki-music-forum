ae057471c1196b84e57871214ef8ccd04802f24f
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-10-13 09:20:41 +0800

    add: add the data type

---
+++ types/data.d.ts
@@ -0,0 +1,34 @@
+export interface Music {
+    id: string
+    name: string
+    singer: Array<string>
+    path: string
+    duration: number
+    avater: string
+    lyric?: string
+    video?: Video
+}
+
+export interface MusicList {
+    id:string,
+    avater: string,
+    name:string,
+    datas:Array<Music>
+}
+
+export interface User {
+    id:string,
+    avater: string
+    email: string
+    name:string,
+    $default:MusicList,
+    $love:MusicList,
+    $list:Array<MusicList>
+}
+export interface Video {
+    id: string
+    name: string
+    singer: string[]
+    avater: string
+    url: string
+}
