d601350912861abea11e7950216afbe44b7e97fa
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-10-13 09:30:19 +0800

    fix: use the retry Requestor by fetch

--- lib/mitsuki-request-lib/request-bus/index.ts
+++ lib/mitsuki-request-lib/request-bus/index.ts
@@ -1,12 +1,12 @@
 import { createFetchRequestor } from "../request-imp/request-fetch-imp";
+// import { createAxiosRequestor } from "../request-imp/request-axios-imp";
+// import { createXHRRequestor } from "../request-imp/request-xhr-imp";
 import { inject,useRequestor,createRetryRequestor,createCacheRequestor,createSerialRequestor,createParallelRequestor,createIdempotentRequestor, } from "../request-core/index";
 
-inject(createFetchRequestor({ baseURL:"http://localhost:3000/api" }))
-inject(createCacheRequestor({
-    key:(config) => config.url,
-    duration:1000,
-    persist:false,
-}))
+
+// inject(createAxiosRequestor({ baseURL:"http://localhost:3000" }))
+inject(createFetchRequestor({ baseURL:"http://localhost:3000" }))
+// inject(createFetchRequestor({ baseURL:"http://localhost:3000" }))
 inject(createRetryRequestor({ max:3 }))
 
 export const req = useRequestor()
