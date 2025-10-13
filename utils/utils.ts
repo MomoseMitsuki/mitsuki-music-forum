f8e8cafee78e31c2938b9169bfa21d051f0af6f9
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-10-13 09:26:21 +0800

    add: add the debounce function

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,9 +1,20 @@
+import type { Music } from "~/types"
+
 export function debounce(fn:VoidFunction, delay = 300) {
     let timer:NodeJS.Timeout | undefined = void 0
-    return function (...args:any[]) {
+    return function () {
         clearTimeout(timer)
         timer = setTimeout(() => {
             fn()
         }, delay)
     }
+}
+
+export function isInList(d:Music,$list:Array<Music>): boolean {
+    for (const item of $list) {
+        if (item.id === d.id) {
+            return true
+        }
+    }
+    return false
 }
