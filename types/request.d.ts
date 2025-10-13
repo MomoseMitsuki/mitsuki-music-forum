2a89b5af52c7fbf7af688db5e2db8609c1b39632
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-10-13 09:22:35 +0800

    add: add the request data type

---
+++ types/request.d.ts
@@ -0,0 +1,31 @@
+export interface AddMusicRequest {
+    name: string
+    path: string
+    singer: string[]
+    duration: number
+    avater: string
+    video?:AddVideoRequest
+    lyric?:AddLyricRequest
+}
+
+export interface AddVideoRequest {
+    musicName: string
+    avater: string
+    url: string
+}
+
+export interface AddLyricRequest {
+    musicName: string
+    url: string
+}
+
+export interface CreateUserListRequest {
+    id: string
+    name: string
+}
+
+export interface AddOneMusicToListRequest {
+    userId: string
+    musicId: string
+    name: string
+}
