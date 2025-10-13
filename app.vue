c3088d276bd2e662af16a50f8cd1ed91d079b6fd
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-10-13 09:27:46 +0800

    add: use the useAsyncData to get the data in SSR

--- app.vue
+++ app.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
 import MusicController from "~/components/MusicController.vue"
 import ForumNav from "~/components/ForumNav.vue"
-import { getPlayListService } from "~/apis/user"
-getPlayListService()
+import { getUserInfoService } from "./apis/user"
+const uiStatusStore = useUiStatusStore()
+const userStore = useUserStore() 
+const { data } = await useAsyncData(
+    "user",
+    () => getUserInfoService("68e8f3a670e1d7faa23026e5"),
+    {
+        default:() => ({
+            ...userStore.user,
+            $list: userStore.$lists,
+            $default:[],
+            $love:[],
+        })
+    }
+)
+const initUser = () => {
+    userStore.$lists = data.value.$list
+    userStore.user.id = data.value.id
+    userStore.user.name = data.value.name
+    userStore.user.avater = data.value.avater
+    userStore.user.email = data.value.email
+}
+initUser()
 </script>
 
 <template>
-    <div class="main">
+    <div class="main" ref="main">
         <ForumNav />
         <NuxtPage />
         <MusicController />
@@ -32,6 +53,24 @@ getPlayListService()
         z-index: -1;
         @include useTheme {
             background-image: getVar("main");
+        }
+    }
+}
+.add-to-playlist {
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 1000;
+    will-change: transform;
+    transition:0.6s linear;
+    svg {
+        display: block;
+        width: 20px;
+        height: 20px;
+        will-change: transform;
+        transition: 0.6s cubic-bezier(0.5,-0.5,1,1);
+        @include useTheme {
+            fill: getVar("textColor")
         }
     }
 }
