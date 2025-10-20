82fbaa32ab4fe27931f35c7ea85eb20b737a6615
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-10-21 00:23:19 +0800

    add: add the postcss

---
+++ postcss.config.mjs
@@ -0,0 +1,9 @@
+import postcssPresetEnv from 'postcss-preset-env';
+
+export default {
+    plugins: {
+        'postcss-preset-env': postcssPresetEnv({
+            autoprefixer: { grid: true }
+        })
+    }
+}
