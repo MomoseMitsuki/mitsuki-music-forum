7a2d6b630cc0e64da29ef3408d65bfe820a9d97b
百濑美月 <15230787+MomoseMitsuki@user.noreply.gitee.com>
2025-10-24 11:36:38 +0800

    fix: use the prisma  single instance

--- server/model/index.ts
+++ server/model/index.ts
@@ -1,15 +1,17 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "~/app/generated/prisma"
 const prisma = new PrismaClient()
 
 async function connectDB() {
     try {
-      await prisma.$connect()
-      console.log(`✅ MongoDB: ${process.env.DATABASE_URL} connection successful!`)
+        await prisma.$connect()
+        console.log(
+            `✅ MongoDB: ${process.env.DATABASE_URL} connection successful!`
+        )
     } catch (err) {
-      console.error('❌ Prisma connection error:', err)
+        console.error("❌ Prisma connection error:", err)
     }
 }
-  
+
 connectDB()
-  
+
 export default prisma
